669105ffdf4c42b70381fede82c64ed2ef3fb3cb
Robyn Kwok <robyn.kwok@duke.edu>
2017-05-18 17:23:30 +0800

    Complete

--- functions.js
+++ functions.js
@@ -1,57 +1,86 @@
 // Question 1
-function maxOfTwoNumbers() {
-
+function maxOfTwoNumbers(i,j) {
+  if (i >= j) return i;
+  else return j;
 }
 
 // Question 2
-function maxOfThree() {
-
+function maxOfThree(i,j,k) {
+  return maxOfTwoNumbers(i, maxOfTwoNumbers(j,k));
 }
 
 // Question 3
-function isCharacterAVowel() {
-
+function isCharacterAVowel(c) {
+  var vowel = ["a", "e", "i", "o", "u"];
+  return vowel.indexOf(c.toLowerCase()) !== -1;
 }
 
 // Question 4
-function sumArray() {
- 
+function sumArray(arr) {
+  var sum = 0;
+  for (var i=0;i<arr.length;i++) {
+    sum += arr[i];
+  }
+  return sum;
 }
-
 
 // Question 4
-function multiplyArray() {
-
+function multiplyArray(arr) {
+  var mult = 0;
+  for (var i=0;i<arr.length;i++) {
+    mult *= arr[i];
+  }
+  return mult;
 }
-
 
 // Question 5
-var reverseString = function (){
-  
+var reverseString = function (s){
+  return s.split("").reverse().join("");
 };
 
 
-// Question 8
-function findLongestWord () {
-  
+// Question 6
+function findLongestWord (arr) {
+  var maxLength = 0;
+  for (var i=0;i<arr.length;i++) {
+    if (arr[i].length > maxLength) {
+      maxLength = arr[i].length;
+    }
+  }
+  return maxLength;
 }
 
-// Question 9
-function filterLongWords () {
-  
+// Question 7
+function filterLongWords (arr, i) {
+  var ret = [];
+  arr.forEach(function(el, index){
+    if (el.length > i) {
+      ret += [el];
+    }
+  });
+  return ret;
 }
-
 
 // Bonus 1
-//??????
-
+String.prototype.reverseString = function () {
+  return reverseString(this);
+};
 
 // Bonus 2
-function charactersOccurencesCount() {
-  
+function charactersOccurencesCount(s) {
+  var ret = {};
+  s.split("").forEach(function(el, index) {
+    if (el in ret) {
+      ret[el] += 1;
+    }
+    else {
+      ret[el] = 1;
+    }
+  });
+  return ret;
 }
 
 // Bonus 3
 var numberOfArguments = function(){
-  
+  return arguments.legth;
 }
